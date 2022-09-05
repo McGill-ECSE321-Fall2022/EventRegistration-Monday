e646f97e48f28ee9b204edc2126ca67271776dca
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2022-09-04 20:21:37 -0400

    Add hello world endpoint

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -2,7 +2,10 @@ package ca.mcgill.ecse321.eventregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EventRegistrationApplication {
 
@@ -10,4 +13,8 @@ public class EventRegistrationApplication {
 		SpringApplication.run(EventRegistrationApplication.class, args);
 	}
 
+	@RequestMapping
+	public String test() {
+		return "Hello there!";
+	}
 }
