ec3177c817393fdd37694cfa3e340c32d6a1d9ca
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2022-11-21 13:33:37 -0500

    Unset CORS policy

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/EventController.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/EventController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +18,6 @@ import ca.mcgill.ecse321.eventregistration.model.Event;
 import ca.mcgill.ecse321.eventregistration.service.EventService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8087")
 public class EventController {
 
 	@Autowired
--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/RegistrationController.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/RegistrationController.java
@@ -3,7 +3,6 @@ package ca.mcgill.ecse321.eventregistration.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,6 @@ import ca.mcgill.ecse321.eventregistration.dto.RegistrationResponseDto;
 import ca.mcgill.ecse321.eventregistration.service.RegistrationService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8087")
 public class RegistrationController {
 
 	@Autowired
