c0e8ec05e4b2d1b14c6beed1eff64104269e2a97
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2022-11-21 11:47:29 -0500

    Create static component

---
+++ EventRegistration-Frontend/src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Event registration</h1>
+    <p>Welcome to the event registration app!</p>
+    <h2>Users</h2>
+    <table>
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <td>Alice</td>
+      </tr>
+      <tr>
+        <td>Bob</td>
+      </tr>
+    </table>
+    <h2>Events</h2>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Date</th>
+        <th>Start Time</th>
+        <th>End Time</th>
+      </tr>
+      <tr>
+        <td>Order 66</td>
+        <td>2022-11-21</td>
+        <td>11:00:00</td>
+        <td>13:00:00</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
